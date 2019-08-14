a1ff4c3b1e0f00407794b860b79257cfc43b61b3
youngalou <youngalou@gmail.com>
2019-08-14 13:49:28 -0700

    Added training with experience buffer. Fully loaded gpu is WAY faster.

--- cartpole/dqn_model.py
+++ cartpole/dqn_model.py
@@ -13,6 +13,7 @@ class DQN_Model(tf.keras.models.Model):
         self.q_layer = layers.Dense(num_actions, name='output')
 
         self.step(np.zeros(input_shape))
+        self.loss = tf.keras.losses.MeanSquaredError()
         self.opt = tf.optimizers.Adam(learning_rate)
 
         self.num_actions = num_actions
@@ -41,3 +42,65 @@ class DQN_Model(tf.keras.models.Model):
         if sample > 1 - epsilon:
             return np.random.randint(self.num_actions)
         return self.step(inputs)
+
+class ExperienceBuffer():
+    def __init__(self, max_size):
+        self.obs, self.actions, self.rewards, self.next_obs, self.next_mask = \
+            None, None, None, None, None
+        
+        self._obs, self._actions, self._rewards, self._next_obs, self._next_mask = \
+            None, None, None, None, None
+        
+        self.max_size = max_size
+        self.size = 0
+        self.remainder = 0
+
+    def add_trajectory(self, obs, actions, rewards, num_steps):
+        next_obs = np.roll(obs, shift=-1, axis=0)
+        next_mask = np.ones(num_steps)
+        next_mask[-1] = 0
+
+        if self.size >= self.max_size:
+            self.reset()
+
+        new_size = self.size + obs.size
+        if new_size > self.max_size:
+            obs, actions, rewards, next_obs, next_mask = \
+                self.split_remainder(obs, actions, rewards, next_obs, next_mask)
+            new_size = self.max_size
+
+        self.append(obs, actions, rewards, next_obs, next_mask)
+        self.size = new_size
+
+    def append(self, obs, actions, rewards, next_obs, next_mask):
+        if self.size == 0:
+            self.obs, self.actions, self.rewards, self.next_obs, self.next_mask = \
+                obs, actions, rewards, next_obs, next_mask
+        else:
+            self.obs = np.append(self.obs, obs, axis=0)
+            self.actions = np.append(self.actions, actions, axis=0)
+            self.rewards = np.append(self.rewards, rewards, axis=0)
+            self.next_obs = np.append(self.next_obs, next_obs, axis=0)
+            self.next_mask = np.append(self.next_mask, next_mask, axis=0)
+    
+    def split_remainder(self, obs, actions, rewards, next_obs, next_mask):
+        remaining_capacity = self.max_size - self.size
+        split = int(remaining_capacity / np.prod(self.obs.shape[1:]))
+        obs, self._obs = obs[:split], obs[split:]
+        actions, self._actions = actions[:split], actions[split:]
+        rewards, self._rewards = rewards[:split], rewards[split:]
+        next_obs, self._next_obs = next_obs[:split], next_obs[split:]
+        next_mask, self._next_mask = next_mask[:split], next_mask[split:]
+        self.remainder = self._obs.shape[0]
+        return obs, actions, rewards, next_obs, next_mask
+
+    def reset(self):
+        if self.remainder == 0:
+            self.__init__(self.max_size)
+        else:
+            self.size = self.remainder
+            self.remainder = 0
+            self.obs, self.actions, self.rewards, self.next_obs, self.next_mask = \
+                self._obs, self._actions, self._rewards, self._next_obs, self._next_mask
+            self._obs, self._actions, self._rewards, self._next_obs, self._next_mask = \
+                None, None, None, None, None
