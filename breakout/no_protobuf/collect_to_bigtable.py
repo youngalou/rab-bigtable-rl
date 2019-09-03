eb145a3ed7989a61a2b05df59cb3565df7ed7a15
youngalou <youngalou@gmail.com>
2019-09-03 15:52:53 -0700

    Dynamically deciding max_row_bytes and added time logging to no_protobuf collect.

--- breakout/no_protobuf/collect_to_bigtable.py
+++ breakout/no_protobuf/collect_to_bigtable.py
@@ -12,6 +12,7 @@ from google.oauth2 import service_account
 from protobuf.experience_replay_pb2 import Trajectory, Info
 from models.dqn_model import DQN_Model
 from util.gcp_io import gcp_load_pipeline, gcs_load_weights, cbt_global_iterator
+from util.logging import TimeLogger
 
 import gym
 
@@ -46,8 +47,9 @@ if __name__ == '__main__':
     #INSTANTIATE CBT TABLE AND GCS BUCKET
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     cbt_table, gcs_bucket = gcp_load_pipeline(args.gcp_project_id, args.cbt_instance_id, args.cbt_table_name, args.bucket_id, credentials)
-    cbt_batcher = cbt_table.mutations_batcher(flush_count=args.num_episodes, max_row_bytes=500000000)
-                                                                                          #104857600
+    max_row_bytes = 8 * np.prod(VISUAL_OBS_SPEC) * args.max_steps * args.num_episodes
+    cbt_batcher = cbt_table.mutations_batcher(flush_count=args.num_episodes, max_row_bytes=max_row_bytes)
+
     #INITIALIZE ENVIRONMENT
     print("-> Initializing Gym environement...")
     env = gym.make('Breakout-v0')
@@ -64,11 +66,24 @@ if __name__ == '__main__':
     global_i = cbt_global_iterator(cbt_table)
     print("global_i = {}".format(global_i))
 
+    #INITIALIZE EXECUTION TIME LOGGER
+    if args.log_time is True:
+        time_logger = TimeLogger(["Load Weights     ",
+                                  "Run Environment  ",
+                                  "Data To Bytes    ",
+                                  "Write Cells      ",
+                                  "Mutate Rows      "])
+
     #COLLECT DATA FOR CBT
     print("-> Starting data collection...")
     rows = []
     for cycle in range(args.num_cycles):
+        if args.log_time is True: time_logger.reset()
+
         gcs_load_weights(model, gcs_bucket, args.prefix, args.tmp_weights_filepath)
+
+        if args.log_time is True: time_logger.log("Load Weights     ")
+
         for i in tqdm(range(args.num_episodes), "Cycle {}".format(cycle)):
 
             #RL LOOP GENERATES A TRAJECTORY
@@ -87,10 +102,14 @@ if __name__ == '__main__':
 
                 if done: break
                 obs = np.asarray(new_obs / 255).astype(np.float32)
+        
+            if args.log_time is True: time_logger.log("Run Environment  ")
 
             observations = np.asarray(observations).flatten().tobytes()
             actions = np.asarray(actions).astype(np.uint8).tobytes()
             rewards = np.asarray(rewards).astype(np.float32).tobytes()
+
+            if args.log_time is True: time_logger.log("Data To Bytes    ")
 
             # SET CELLS WITH DEFAULT PYTHON ENCODING
             row_key_i = i + global_i + (cycle * args.num_episodes)
@@ -106,6 +125,8 @@ if __name__ == '__main__':
                          column='rewards',
                          value=rewards)
             rows.append(row)
+
+            if args.log_time is True: time_logger.log("Write Cells      ")
         
         #UPDATE GLOBAL ITERATOR
         gi_row = cbt_table.row('global_iterator'.encode())
@@ -119,6 +140,11 @@ if __name__ == '__main__':
         cbt_batcher.mutate_rows(rows)
         cbt_batcher.flush()
         rows = []
+
+        if args.log_time is True: time_logger.log("Mutate Rows      ")
+
         print("-> Saved trajectories {} - {}.".format(row_key_i - (args.num_episodes-1), row_key_i))
+
+        if args.log_time is True: time_logger.print_totaltime_logs()
     env.close()
     print("-> Done!")
