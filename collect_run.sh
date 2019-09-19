7a9adee72fd9cbe1cddeec4d25790ae6e609b47a
pootitan <pootitan@gmail.com>
2019-09-18 21:12:57 -0700

    update docker_run

---
+++ collect_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export COLLECTION_PY=crane.collect
+export ENV_FILENAME=CraneML_0813.x86_64
+
+echo "==> Host name: " $HOSTNAME
+echo "==> collect python module: " $COLLECTION_PY 
+echo "==> env file: " $ENV_FILENAME
+
+python3 -m \
+    $COLLECTION_PY \
+    --docker-training=True \
+    --env-filename=$ENV_FILENAME
