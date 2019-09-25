e6f8f4c34bfcded2ffa2e5af7552119f321e4f81
youngalou <youngalou@gmail.com>
2019-09-25 21:23:50 +0000

    Trimmed excess code from unity_env and added support for visual and vector obs.

--- util/unity_env.py
+++ util/unity_env.py
@@ -11,13 +11,10 @@ class UnityGymException(error.Error):
     """
     Any error related to the gym wrapper of ml-agents.
     """
-
     pass
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("gym_unity")
-
 
 class UnityEnvironmentWrapper(gym.Env):
     """
@@ -31,8 +28,7 @@ class UnityEnvironmentWrapper(gym.Env):
         environment_filename: str,
         worker_id: int = 0,
         use_visual: bool = False,
-        uint8_visual: bool = False,
-        multiagent: bool = False,
+        use_vector: bool = False
         flatten_branched: bool = False,
         no_graphics: bool = False,
         allow_multiple_visual_obs: bool = False,
@@ -43,8 +39,6 @@ class UnityEnvironmentWrapper(gym.Env):
         :param environment_filename: The UnityEnvironment path or file to be wrapped in the gym.
         :param worker_id: Worker number for environment.
         :param use_visual: Whether to use visual observation or vector observation.
-        :param uint8_visual: Return visual observations as uint8 (0-255) matrices instead of float (0.0-1.0).
-        :param multiagent: Whether to run in multi-agent mode (lists of obs, reward, done).
         :param flatten_branched: If True, turn branched discrete action spaces into a Discrete space rather than
             MultiDiscrete.
         :param no_graphics: Whether to run the Unity simulator in no-graphics mode
@@ -58,13 +52,8 @@ class UnityEnvironmentWrapper(gym.Env):
         )
         self.name = self._env.academy_name
         self.visual_obs = None
-        self._current_state = None
-        self._n_agents = None
-        self._multiagent = multiagent
+        self.vector_obs = None
         self._flattener = None
-        self.game_over = (
-            False
-        )  # Hidden flag used by Atari environments to determine if the game is over
         self._allow_multiple_visual_obs = allow_multiple_visual_obs
 
         # Check brain configuration
@@ -88,14 +77,6 @@ class UnityEnvironmentWrapper(gym.Env):
             )
         self.use_visual = brain.number_visual_observations >= 1 and use_visual
 
-        if not use_visual and uint8_visual:
-            logger.warning(
-                "`uint8_visual was set to true, but visual observations are not in use. "
-                "This setting will not have any effect."
-            )
-        else:
-            self.uint8_visual = uint8_visual
-
         if brain.number_visual_observations > 1 and not self._allow_multiple_visual_obs:
             logger.warning(
                 "The environment contains more than one visual observation. "
@@ -109,10 +90,6 @@ class UnityEnvironmentWrapper(gym.Env):
                 "if it is wrapped in a gym."
             )
 
-        # Check for number of agents in scene.
-        initial_info = self._env.reset()[self.brain_name]
-        self._check_agents(len(initial_info.agents))
-
         # Set observation and action spaces
         if brain.vector_action_space_type == "discrete":
             if len(brain.vector_action_space_size) == 1:
@@ -125,7 +102,6 @@ class UnityEnvironmentWrapper(gym.Env):
                     self._action_space = spaces.MultiDiscrete(
                         brain.vector_action_space_size
                     )
-
         else:
             if flatten_branched:
                 logger.warning(
@@ -160,15 +136,9 @@ class UnityEnvironmentWrapper(gym.Env):
         Returns: observation (object/list): the initial observation of the
             space.
         """
-        info = self._env.reset()[self.brain_name]
-        n_agents = len(info.agents)
-        self._check_agents(n_agents)
-        self.game_over = False
+        current_state = self._env.reset()[self.brain_name]
 
-        if not self._multiagent:
-            obs, reward, done, info = self._single_step(info)
-        else:
-            obs, reward, done, info = self._multi_step(info)
+        obs, reward, done, info = self._single_step(current_state)
         return obs
 
     def step(self, action):
@@ -187,90 +157,32 @@ class UnityEnvironmentWrapper(gym.Env):
         """
 
         # Use random actions for all other agents in environment.
-        if self._multiagent:
-            if not isinstance(action, list):
-                raise UnityGymException(
-                    "The environment was expecting `action` to be a list."
-                )
-            if len(action) != self._n_agents:
-                raise UnityGymException(
-                    "The environment was expecting a list of {} actions.".format(
-                        self._n_agents
-                    )
-                )
-            else:
-                if self._flattener is not None:
-                    # Action space is discrete and flattened - we expect a list of scalars
-                    action = [self._flattener.lookup_action(_act) for _act in action]
-                action = np.array(action)
-        else:
-            if self._flattener is not None:
-                # Translate action into list
-                action = self._flattener.lookup_action(action)
+        if self._flattener is not None:
+            # Translate action into list
+            action = self._flattener.lookup_action(action)
 
-        info = self._env.step(action)[self.brain_name]
-        n_agents = len(info.agents)
-        self._check_agents(n_agents)
-        self._current_state = info
+        current_state = self._env.step(action)[self.brain_name]
 
-        if not self._multiagent:
-            obs, reward, done, info = self._single_step(info)
-            self.game_over = done
-        else:
-            obs, reward, done, info = self._multi_step(info)
-            self.game_over = all(done)
+        obs, reward, done, info = self._single_step(current_state)
         return obs, reward, done, info
 
-    def _single_step(self, info):
+    def _single_step(self, current_state):
         if self.use_visual:
-            visual_obs = info.visual_observations
+            visual_obs = current_state.visual_observations
 
             if self._allow_multiple_visual_obs:
-                visual_obs_list = []
-                for obs in visual_obs:
-                    visual_obs_list.append(self._preprocess_single(obs[0]))
+                visual_obs_list = [self._preprocess_single(obs[0]) for obs in visual_obs]
                 self.visual_obs = np.moveaxis(np.concatenate(visual_obs_list, axis=0), 0, -1)
             else:
                 self.visual_obs = self._preprocess_single(visual_obs[0][0])
 
-            default_observation = self.visual_obs
-        else:
-            default_observation = info.vector_observations[0, :]
+        if self.use_vector:
+            self.vector_obs = current_state.vector_observations[0, :]
 
-        return (
-            default_observation,
-            info.rewards[0],
-            info.local_done[0],
-            {"text_observation": info.text_observations[0], "brain_info": info},
-        )
+        return self.visual_obs, self.vector_obs, current_state.rewards[0], current_state.local_done[0]
 
     def _preprocess_single(self, single_visual_obs):
-        if self.uint8_visual:
-            return (255.0 * single_visual_obs).astype(np.uint8)
-        else:
-            return single_visual_obs
-
-    def _multi_step(self, info):
-        if self.use_visual:
-            self.visual_obs = self._preprocess_multi(info.visual_observations)
-            default_observation = self.visual_obs
-        else:
-            default_observation = info.vector_observations
-        return (
-            list(default_observation),
-            info.rewards,
-            info.local_done,
-            {"text_observation": info.text_observations, "brain_info": info},
-        )
-
-    def _preprocess_multi(self, multiple_visual_obs):
-        if self.uint8_visual:
-            return [
-                (255.0 * _visual_obs).astype(np.uint8)
-                for _visual_obs in multiple_visual_obs
-            ]
-        else:
-            return multiple_visual_obs
+        return single_visual_obs.astype(np.float32)
 
     def render(self, mode='rgb_array'):
         img = self.visual_obs
@@ -291,33 +203,6 @@ class UnityEnvironmentWrapper(gym.Env):
 
     def get_action_meanings(self):
         return self.action_meanings
-
-    def seed(self, seed=None):
-        """Sets the seed for this env's random number generator(s).
-        Currently not implemented.
-        """
-        logger.warn("Could not seed environment %s", self.name)
-        return
-
-    def _check_agents(self, n_agents):
-        if not self._multiagent and n_agents > 1:
-            raise UnityGymException(
-                "The environment was launched as a single-agent environment, however"
-                "there is more than one agent in the scene."
-            )
-        elif self._multiagent and n_agents <= 1:
-            raise UnityGymException(
-                "The environment was launched as a mutli-agent environment, however"
-                "there is only one agent in the scene."
-            )
-        if self._n_agents is None:
-            self._n_agents = n_agents
-            logger.info("{} agents within environment.".format(n_agents))
-        elif self._n_agents != n_agents:
-            raise UnityGymException(
-                "The number of agents in the environment has changed since "
-                "initialization. This is not supported."
-            )
 
     @property
     def metadata(self):
