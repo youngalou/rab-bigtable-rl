05efd9dca0440b267af60819e4f5266854abd6bd
youngalou <youngalou@gmail.com>
2019-09-27 21:17:24 +0000

    Updated evaluation for dual obs arm env.

--- crane/evaluate.py
+++ crane/evaluate.py
@@ -1,37 +1,36 @@
 import argparse
 import time
 from tqdm import tqdm
-
 import numpy as np
 
 from google.oauth2 import service_account
 
 from models.dqn_model import DQN_Model
 from util.gcp_io import gcs_load_bucket, gcs_load_weights
-
-import gym
+from util.unity_env import UnityEnvironmentWrapper
 
 #SET API CREDENTIALS
 SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
 SERVICE_ACCOUNT_FILE = 'cbt_credentials.json'
 
 #MODEL HYPERPARAMETERS
-VISUAL_OBS_SPEC = [224,224,3]
-VECTOR_OBS_SPEC = [10]
-NUM_ACTIONS=6
+VISUAL_OBS_SPEC = [224,224,2]
+VECTOR_OBS_SPEC = [29]
+NUM_ACTIONS=8
 CONV_LAYER_PARAMS=((8,4,32),(4,2,64),(3,1,64))
 FC_LAYER_PARAMS=(512,)
 
 if __name__ == '__main__':
     #COMMAND-LINE ARGUMENTS
-    parser = argparse.ArgumentParser('Crane Evaluation Script')
+    parser = argparse.ArgumentParser('RAB Evaluation Script')
     parser.add_argument('--gcp-project-id', type=str, default='for-robolab-cbai')
     parser.add_argument('--bucket-id', type=str, default='youngalou')
-    parser.add_argument('--prefix', type=str, default='crane')
+    parser.add_argument('--prefix', type=str, default='crane-dualobs')
     parser.add_argument('--tmp-weights-filepath', type=str, default='/tmp/model_weights_tmp.h5')
     parser.add_argument('--num-cycles', type=int, default=1000)
     parser.add_argument('--num-episodes', type=int, default=10)
     parser.add_argument('--max-steps', type=int, default=100)
+    parser.add_argument('--env-filename', type=str, default='envs/RAB-Arms/RabRobotArm_0930_V1/RabRobotArm_0930_V1.app')
     args = parser.parse_args()
 
     #INSTANTIATE CBT TABLE AND GCS BUCKET
@@ -41,13 +40,13 @@ if __name__ == '__main__':
     #LOAD MODEL
     model = DQN_Model(input_shape=VECTOR_OBS_SPEC,
                       num_actions=NUM_ACTIONS,
-                      fc_layer_params=FC_LAYER_PARAMS,
-                      learning_rate=LEARNING_RATE)
+                      fc_layer_params=FC_LAYER_PARAMS)
 
     #INITIALIZE ENVIRONMENT
     print("-> Initializing Crane environement...")
     env = UnityEnvironmentWrapper(environment_filename=args.env_filename,
                                   use_visual=True,
+                                  use_vector=True,
                                   docker_training=False)
     print("-> Environment intialized.")
 
@@ -57,8 +56,7 @@ if __name__ == '__main__':
         gcs_load_weights(model, gcs_bucket, args.prefix, args.tmp_weights_filepath)
         total_reward = 0
         for i in tqdm(range(args.num_episodes), "Cycle {}".format(cycle)):
-            observations, actions, rewards = [], [], []
-            obs = np.array(env.reset())
+            obs = env.reset()
             reward = 0
             done = False
             
@@ -67,15 +65,11 @@ if __name__ == '__main__':
                 time.sleep(.05)
 
                 action = model.step(obs)
-                new_obs, reward, done, info = env.step(action)
-
-                observations.append(obs)
-                actions.append(action)
-                rewards.append(reward)
+                new_obs, reward, done = env.step(action)
 
                 total_reward += reward
                 if done: break
-                obs = np.array(new_obs)
+                obs = new_obs
         print("Average reward: {}".format(total_reward / args.num_episodes))
     env.close()
     print("-> Done!")
